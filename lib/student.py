8d83c114454f9477b54268dc95ede56088f230aa
l3xst1ng <howdy.martt@gmail.com>
2024-05-28 22:20:59 +0300

    Completed Object Inheritance Lab Part One

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,9 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []  # Initializing knowledge as an empty list
+        
+    def learn(self, knowledge):
+        self.knowledge.append(knowledge)
